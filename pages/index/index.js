fa499a34bf3b9eca9954f02bbacfa35e636e2d75
HuaHua <3118590779@qq.com>
2023-08-14 17:19:27 +0800

    首页模板完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,18 +3,42 @@ const app = getApp();
 Page({
   data: {
     navBarFullHeight: 0, // 整个导航栏高度
-    swiperList: [],
+    swiperList: [], //首页轮播图的数据列表
+    Classifieds: [], //首页分类推荐的数据列表
+    bulletins: [], //首页公告
+    WaterfallRecommendationList: [], // 首页瀑布流推荐
   },
   async onLoad() {
     this.setData({ navBarFullHeight: app.globalData.navBarFullHeight });
     await app.ajax("/product/querySwiper").then((res) => {
       this.setData({
         swiperList: res.data.data.map((item) => {
-          item.swiper_image = app.https + item.swiper_image;
+          item.getSwiperPic = app.https + item.getSwiperPic;
           return item;
         }),
       });
-      console.log(this.data.swiperList);
     });
+    await app.ajax("/recommend/queryRecommendAll").then((res) => {
+      this.setData({
+        Classifieds: res.data.data.map((item) => {
+          item.recommendIcon = app.https + item.recommendIcon;
+          return item;
+        }),
+      });
+    });
+    await app.ajax("/notice/queryNotice").then((res) => {
+      this.setData({
+        bulletins: res.data.data,
+      });
+    });
+    await app.ajax("/recommend/queryWaterfallsAll").then((res) => {
+      this.setData({
+        WaterfallRecommendationList: res.data.data.map(item=>{
+          item.image = app.https + item.image;
+          return item;
+        }),
+      });
+    });
+    // console.log(this.data.swiperList, this.data.Classifieds);
   },
 });
