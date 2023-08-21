46018305ab1a5f7178f6fafc272b436d12b411de
HuaHua <3118590779@qq.com>
2023-08-21 14:59:30 +0800

    商品详情页完成

--- pages/productDetail/productDetail.js
+++ pages/productDetail/productDetail.js
@@ -5,13 +5,15 @@ Page({
     goodData: {}, //商品的所有文本信息
     goodSwiper: [], //商品的头部轮播图
     activeIndex: 0, //商品的头部轮播图的激活序号
+    productIntro: [], //商品的下方介绍图
   },
+  //轮播图变更时,修改右下方序号
   changeActiveIndex(e) {
     console.log("轮播图切换=>", e);
     this.setData({
       activeIndex: e.detail.current,
     });
-  }, //轮播图变更时,修改右下方序号
+  },
   onLoad(options) {
     this.setData({ navBarFullHeight: app.globalData.navBarFullHeight });
     app
@@ -37,6 +39,28 @@ Page({
         console.log("获取到了商品数据=>", res);
         this.setData({
           goodData: res.data.data,
+        });
+      });
+    app
+      .ajax({
+        path: "/product/queryProductLabel",
+        data: { productId: options.id },
+      })
+      .then((res) => {
+        console.log("获取到了商品标签=>", res);
+      });
+    app
+      .ajax({
+        path: "/product/queryProductIntro",
+        data: { productId: options.id },
+      })
+      .then((res) => {
+        console.log("获取到了商品介绍图=>", res);
+        this.setData({
+          productIntro: res.data.data.map((item) => {
+            item.image = app.globalData.https + item.image;
+            return item;
+          }),
         });
       });
   },
