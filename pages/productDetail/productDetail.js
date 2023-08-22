f4427f9f03c7016ab9e2f895dcc7e9d8714ebb8e
HuaHua <3118590779@qq.com>
2023-08-22 12:06:51 +0800

    商品详情页,底部安全区域修改完毕

--- pages/productDetail/productDetail.js
+++ pages/productDetail/productDetail.js
@@ -7,6 +7,9 @@ Page({
     activeIndex: 0, //商品的头部轮播图的激活序号
     productLabel: [], //商品标签
     productIntro: [], //商品的下方介绍图
+    cartTabbarHeight: 0, //底部"加购/购买"栏的高度
+    pageContainerShow: false, //假页面容器显示状态
+    hiddenTabbarBtn: 0, //是同时展示加购/购买,还是其中一个
   },
   //轮播图变更时,修改右下方序号
   changeActiveIndex(e) {
@@ -15,8 +18,26 @@ Page({
       activeIndex: e.detail.current,
     });
   },
+  // 点击选择规格,并加入购物车/立即购买
+  showPageContainer(e) {
+    this.setData({
+      pageContainerShow: true,
+      hiddenTabbarBtn: e.currentTarget.dataset.status,
+    });
+    console.log("点击显示规格弹窗", e, this.data.hiddenTabbarBtn);
+  },
   onLoad(options) {
     this.setData({ navBarFullHeight: app.globalData.navBarFullHeight });
+    // 查询底部"加购/购买"栏高度为多少
+    let query = wx.createSelectorQuery();
+    query
+      .select(".cart-tabbar")
+      .boundingClientRect((rect) => {
+        this.setData({
+          cartTabbarHeight: rect.height,
+        });
+      })
+      .exec();
     // 获取商品轮播图
     app
       .ajax({
@@ -41,7 +62,7 @@ Page({
       .then((res) => {
         console.log("获取到了商品数据=>", res);
         this.setData({
-          goodData: res.data.data,
+          goodData: res.data.data.productDetails,
         });
       });
     // 获取商品标签
